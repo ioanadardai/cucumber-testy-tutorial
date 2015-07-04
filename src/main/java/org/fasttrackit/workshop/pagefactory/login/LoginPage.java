b7a07ce8b57c9df7011f536e113fb6886a60b9a9
ioanadardai <ioana.dardai@pitechnologies.ro>
2015-07-04 11:49:36 +0300

    create loginPage.errorMessageShouldBePresent method

--- src/main/java/org/fasttrackit/workshop/pagefactory/login/LoginPage.java
+++ src/main/java/org/fasttrackit/workshop/pagefactory/login/LoginPage.java
@@ -1,12 +1,13 @@
 package org.fasttrackit.workshop.pagefactory.login;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class LoginPage {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginPage.class);
@@ -20,6 +21,8 @@ public class LoginPage {
     @FindBy(how = How.ID, using = "password")
     private WebElement password;
 
+    @FindBy(how = How.CLASS_NAME, using = "error-msg")
+    private WebElement error;
 
     public void clickOnLoginButton() {
         loginButton.click();
@@ -28,5 +31,9 @@ public class LoginPage {
     public void enterCredentials(String emailValue, String passValue) {
         email.sendKeys(emailValue);
         password.sendKeys(passValue);
+    }
+
+    public void errorMessageShouldBePresent(String expectedMessage) {
+        assertThat(error.getText(), is(expectedMessage));
     }
 }
