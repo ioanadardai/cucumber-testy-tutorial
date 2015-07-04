4a357643e541f580eb2b397071ac74c740577a21
ioanadardai <ioana.dardai@pitechnologies.ro>
2015-07-04 11:08:45 +0300

    first page object model

--- src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
+++ src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
@@ -9,6 +9,7 @@ import org.fasttrackit.util.TestBaseNative;
 import org.fasttrackit.workshop.pagefactory.login.LoginPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +19,15 @@ import static org.hamcrest.core.Is.is;
 public class LoginSteps extends TestBaseNative {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginSteps.class);
 
-    LoginPage loginPage = new LoginPage();
+    LoginPage loginPage;
+
+    public LoginSteps() {
+        initPage();
+    }
+
+    public void initPage() {
+        loginPage = PageFactory.initElements(driver, LoginPage.class);
+    }
 
     @Given("^I access the login page$")
     public void I_access_the_login_page() {
@@ -32,7 +41,7 @@ public class LoginSteps extends TestBaseNative {
 
     @When("^I click on login button$")
     public void I_click_on_login_button() {
-        loginPage.clickOnLoginButton(driver);
+        loginPage.clickOnLoginButton();
     }
 
     @Then("^I check if user was logged in$")
